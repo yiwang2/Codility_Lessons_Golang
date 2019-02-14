7bb855a92c27ced7c332e5e840c9aa7be41e8fcc
yi <lixinandme@gmail.com>
2019-02-14 11:17:15 +0000

    Update lesson7_Nesting.go

--- lesson7_Nesting.go
+++ lesson7_Nesting.go
@@ -3,24 +3,23 @@ package main
 import "./stack"
 
 func Nesting_Solution(S string) int {
-	var runeS []rune = []rune(S);
 	var sLength int  = len(S);
 	var s *stack.Stack = stack.New();
 	if (sLength == 0) {
 		return 1;
 	}
 
-	for i :=0; i < sLength; i ++ {
-		var val = string(runeS[i]);
-		if (val == "(") {
-			s.Push(1)
-		} else if (val == ")") {
+	for _, val :=range (S) {
+		var val1 = string(val)
+		if (val1 == "(") {
+			s.Push(val1)
+		} else if (val1 == ")") {
 			if (s.Len() == 0) {
 				return 0;
 			}
 
-			var value int = s.Pop().(int);
-			if (value -1 != 0) {
+			var value string = s.Pop().(string);
+			if (value != "(") {
 				return 0;
 			}
 		}
