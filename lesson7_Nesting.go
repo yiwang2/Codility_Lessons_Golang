88dafc3ae55a383e170188add08b48a96029828c
yi <lixinandme@gmail.com>
2019-02-14 11:58:24 +0000

    Update lesson7_Nesting.go

--- lesson7_Nesting.go
+++ lesson7_Nesting.go
@@ -2,6 +2,39 @@ package main
 
 import "./stack"
 
+func Nesting_Solution2(S string) int {
+	var sLength int  = len(S);
+	var s []int= []int{};
+	if (sLength == 0) {
+		return 1;
+	}
+
+	for _, val :=range (S) {
+		var val1 = string(val)
+		if (val1 == "(") {
+			s = append(s, 1);
+		} else if (val1 == ")") {
+			var ss int = len(s);
+			if (ss == 0) {
+				return 0;
+			}
+
+			if (s[ss-1] -1 == 0) {
+				s= s[0:ss-1];
+			} else {
+				return 0;
+			}
+		}
+	}
+
+	if (len(s) != 0) {
+		return 0;
+	}
+
+	return 1;
+}
+
+//87%   <= performance 75%  0.13 required 0.10
 func Nesting_Solution(S string) int {
 	var sLength int  = len(S);
 	var s *stack.Stack = stack.New();
