497b6b70ca4f3b8af87cee0aa4ff80cc097050a5
yi <lixinandme@gmail.com>
2019-02-14 10:42:57 +0000

    stack pack

--- lesson7_Brackets.go
+++ lesson7_Brackets.go
@@ -1,51 +1,7 @@
 package main
 
 import "fmt"
-
-//go get github.com/golang-collections/collections
-type (
-	Stack struct {
-		top *node
-		length int
-	}
-	node struct {
-		value interface{}
-		prev *node
-	}
-)
-// Create a new stack
-func New() *Stack {
-	return &Stack{nil,0}
-}
-// Return the number of items in the stack
-func (this *Stack) Len() int {
-	return this.length
-}
-// View the top item on the stack
-func (this *Stack) Peek() interface{} {
-	if this.length == 0 {
-		return nil
-	}
-	return this.top.value
-}
-// Pop the top item of the stack and return it
-func (this *Stack) Pop() interface{} {
-	if this.length == 0 {
-		return nil
-	}
-
-	n := this.top
-	this.top = n.prev
-	this.length--
-	return n.value
-}
-// Push a value onto the top of the stack
-func (this *Stack) Push(value interface{}) {
-	n := &node{value,this.top}
-	this.top = n
-	this.length++
-}
-
+import  "../stack"
 
 func Brackets_Solution(S string) int {
 
@@ -53,9 +9,7 @@ func Brackets_Solution(S string) int {
 	if (sLength % 2 != 0) {
 		return 0;
 	}
-
-	var s *Stack = New();
-
+	var s *stack.Stack = stack.New();
 	var runeS []rune = []rune(S);
 
 	for i :=0; i < sLength; i ++ {
